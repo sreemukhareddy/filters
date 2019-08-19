28cf02d6bec0c7a0da2db22d437f3b964bc9e572
Praveen Hasthalapuram <praveen.hasthalapuram@macys.com>
2019-08-19 15:12:43 +0530

    asfg

--- src/com/filters/demo/HelloWorld.java
+++ src/com/filters/demo/HelloWorld.java
@@ -28,9 +28,6 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		system.out.println("Hello......");
-		System.out.println("zxcvb");
-		System.out.println("IJKLLLL");
 		response.getWriter().append("<h1>Hello World...</h1> <br />");
 	}
 
